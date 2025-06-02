47da2aa004b8604263805157f46d2e47aab84633
Ednilson Chiziane <eddybruno43@gmail.com>
2025-06-02 15:08:00 +0200

    feat()carload imput

--- src/main/java/com/api/water_sytem_management_java/WaterSytemManagementJavaApplication.java
+++ src/main/java/com/api/water_sytem_management_java/WaterSytemManagementJavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WaterSytemManagementJavaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WaterSytemManagementJavaApplication.class, args);
     }
 
