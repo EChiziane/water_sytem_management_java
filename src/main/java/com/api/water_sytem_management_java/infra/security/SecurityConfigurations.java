bb10e098d2885d3459591bd6a04d6520806f8668
Chiziane <eddybruno43@gmail.com>
2026-01-07 20:36:44 +0200

    refactor: general code cleanup

--- src/main/java/com/api/water_sytem_management_java/infra/security/SecurityConfigurations.java
+++ src/main/java/com/api/water_sytem_management_java/infra/security/SecurityConfigurations.java
@@ -57,6 +57,7 @@ public class SecurityConfigurations {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
                 "https://customer-dashboard-angular-ng-zorro-s2hx.vercel.app",
+                "https://water-sytem-management-angular-r5bc.vercel.app",
                 "https://customer-dashboard-angular-ng-zorro-s2hx-nfyrgjo2y.vercel.app",
                 "https://778b-197-218-60-63.ngrok-free.app"
         ));
